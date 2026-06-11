0784c29f5dc6435669ec3f6079f97baaca0a32a0
Yusuf Karanfil <75080725+Yusuf-Karanfil@users.noreply.github.com>
2026-06-11 09:02:51 +0300

    Add administrator privilege check for script execution

--- AfterFormat/1_surucuden_once.bat
+++ AfterFormat/1_surucuden_once.bat
@@ -7,6 +7,7 @@ if %errorLevel% neq 0 (
     echo Yonetici yetkisi gerekli. Yukseltiliyor...
     powershell -Command "Start-Process '%~f0' -Verb RunAs"
     exit /b
+
 )
 
 powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0files\1_surucuden_once.ps1"
